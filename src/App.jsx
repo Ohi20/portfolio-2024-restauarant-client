2a1730443849e0f856048b87104cc1aa52865559
AshrafOhi <ashrafohi20@gmail.com>
2024-07-28 13:18:31 +0600

    tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <button className="btn btn-active">Default</button>
+      <button className="btn btn-active btn-neutral">Neutral</button>
+      <button className="btn btn-active btn-primary">Primary</button>
+    </>
+  );
+}
+
+export default App;
