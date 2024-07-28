16e7a2b2d53fc3c51331c199f363214073e55509
AshrafOhi <ashrafohi20@gmail.com>
2024-07-28 14:37:05 +0600

    home-page-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <button className="btn btn-active">Default</button>
-      <button className="btn btn-active btn-neutral">Neutral</button>
-      <button className="btn btn-active btn-primary">Primary</button>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
